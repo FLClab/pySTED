1fbcb07d5a2ec7cabe5d370bdbfa39fd1ada2c87
Benoit Turcotte <benoiturcotte@gamil.com>
2021-01-08 13:15:41 -0500

    Added means to sample from a distribution built from multile flash events

--- pysted/utils.py
+++ pysted/utils.py
@@ -745,11 +745,16 @@ def shift_data(data, peak_idx=5):
     :param peak_idx: idx at which we want the peak to be
     :return: The shifted data
     """
-    # probably only works if I shift to the left, haven't tested for shifting to the right
     peak_arg = numpy.argmax(data)
-    shifted_curve = data[peak_arg - peak_idx:]
-    while len(shifted_curve) != 40:
-        shifted_curve = numpy.append(shifted_curve, shifted_curve[-1])
+    if peak_arg >= peak_idx:
+        shifted_curve = data[peak_arg - peak_idx:]
+        while len(shifted_curve) != 40:
+            shifted_curve = numpy.append(shifted_curve, shifted_curve[-1])
+    else:
+        shifted_curve = numpy.zeros(data.shape)
+        shifted_curve[:peak_idx - peak_arg] = data[0]
+        shifted_curve[peak_idx - peak_arg: peak_idx] = data[:peak_arg]
+        shifted_curve[peak_idx:] = data[peak_arg: - (peak_idx - peak_arg)]
     return shifted_curve
 
 
@@ -769,3 +774,87 @@ def get_avg_lightcurve(light_curves):
     avg_shifted_curves = numpy.mean(shifted_curves, axis=0)
     std_shifted_curves = numpy.std(shifted_curves, axis=0)
     return avg_shifted_curves, std_shifted_curves
+
+
+def savitzky_golay(y, window_size, order, deriv=0, rate=1):
+    r"""Smooth (and optionally differentiate) data with a Savitzky-Golay filter.
+    The Savitzky-Golay filter removes high frequency noise from data.
+    It has the advantage of preserving the original shape and
+    features of the signal better than other types of filtering
+    approaches, such as moving averages techniques.
+    Parameters
+    ----------
+    y : array_like, shape (N,)
+        the values of the time history of the signal.
+    window_size : int
+        the length of the window. Must be an odd integer number.
+    order : int
+        the order of the polynomial used in the filtering.
+        Must be less then `window_size` - 1.
+    deriv: int
+        the order of the derivative to compute (default = 0 means only smoothing)
+    Returns
+    -------
+    ys : ndarray, shape (N)
+        the smoothed signal (or it's n-th derivative).
+    Notes
+    -----
+    The Savitzky-Golay is a type of low-pass filter, particularly
+    suited for smoothing noisy data. The main idea behind this
+    approach is to make for each point a least-square fit with a
+    polynomial of high order over a odd-sized window centered at
+    the point.
+    Examples
+    --------
+    t = np.linspace(-4, 4, 500)
+    y = np.exp( -t**2 ) + np.random.normal(0, 0.05, t.shape)
+    ysg = savitzky_golay(y, window_size=31, order=4)
+    import matplotlib.pyplot as plt
+    plt.plot(t, y, label='Noisy signal')
+    plt.plot(t, np.exp(-t**2), 'k', lw=1.5, label='Original signal')
+    plt.plot(t, ysg, 'r', label='Filtered signal')
+    plt.legend()
+    plt.show()
+    References
+    ----------
+    .. [1] A. Savitzky, M. J. E. Golay, Smoothing and Differentiation of
+       Data by Simplified Least Squares Procedures. Analytical
+       Chemistry, 1964, 36 (8), pp 1627-1639.
+    .. [2] Numerical Recipes 3rd Edition: The Art of Scientific Computing
+       W.H. Press, S.A. Teukolsky, W.T. Vetterling, B.P. Flannery
+       Cambridge University Press ISBN-13: 9780521880688
+    """
+
+    # try:
+    #     window_size = numpy.abs(numpy.int(window_size))
+    #     order = numpy.abs(numpy.int(order))
+    # except ValueError, msg:
+    #     raise ValueError("window_size and order have to be of type int")
+    if window_size % 2 != 1 or window_size < 1:
+        raise TypeError("window_size size must be a positive odd number")
+    if window_size < order + 2:
+        raise TypeError("window_size is too small for the polynomials order")
+    order_range = range(order+1)
+    half_window = (window_size -1) // 2
+    # precompute coefficients
+    b = numpy.mat([[k**i for i in order_range] for k in range(-half_window, half_window+1)])
+    m = numpy.linalg.pinv(b).A[deriv] * rate**deriv * math.factorial(deriv)
+    # pad the signal at the extremes with
+    # values taken from the signal itself
+    firstvals = y[0] - numpy.abs( y[1:half_window+1][::-1] - y[0] )
+    lastvals = y[-1] + numpy.abs(y[-half_window-1:-1][::-1] - y[-1])
+    y = numpy.concatenate((firstvals, y, lastvals))
+    return numpy.convolve( m[::-1], y, mode='valid')
+
+
+def sample_light_curve(light_curves):
+    """
+    This function allows to sample from a distribution of light curves and handles the smoothing and correcting values.
+    :param light_curves: list containing the light curves we wish to do stats and sample from
+    :return: A smoothed curve sampled from the distribution
+    """
+    avg_curve, std_curve = get_avg_lightcurve(light_curves)
+    sampled_curve = numpy.random.normal(avg_curve, std_curve)
+    sampled_curve = numpy.where(sampled_curve >= 1, sampled_curve, 1)
+    smoothed_sampled = savitzky_golay(sampled_curve, 5, 2)   # These params give nice curves :)
+    return smoothed_sampled
