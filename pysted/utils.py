38b113d0aec6eaaa382d9688ee68e32068b93e32
Benoit Turcotte <benoiturcotte@gamil.com>
2021-01-15 14:08:48 -0500

    working on a wrapping function for generation of random-ish datamaps. Currently everything works fine except for the fact that it randomly gets stuck in a loop sometimes, trying to figure that out

--- pysted/utils.py
+++ pysted/utils.py
@@ -916,7 +916,7 @@ def generate_fiber_with_synapses(datamap_shape, fibre_min, fibre_max, n_synapses
     return fibre, polygon_list
 
 
-def generate_secondary_fibers(datamap_shape, main_fiber, n_secondary, min_dist=10, sec_len=(2, 6)):
+def generate_secondary_fibers(datamap_shape, main_fiber, n_sec, min_dist=10, sec_len=(2, 6)):
     """
     Similar to the synapse function, but with fibers instead. The goal would be to pass
     an already existing fiber to this func and simply spawn the secondary fibers
@@ -925,6 +925,7 @@ def generate_secondary_fibers(datamap_shape, main_fiber, n_secondary, min_dist=1
     n_added = 0
     sec_fiber_positions = numpy.empty((0, 2))
     angle_at_position = []
+    n_secondary = int(random.uniform(*n_sec))
     while n_added != n_secondary:
         # sampled_node = numpy.asarray(random.sample(list(main_fiber.nodes_position), 1)[0].astype(int))
         sample_idx = numpy.random.randint(len(main_fiber.nodes_position))
@@ -951,7 +952,6 @@ def generate_secondary_fibers(datamap_shape, main_fiber, n_secondary, min_dist=1
                 angle_at_position.append(main_fiber.angles[sample_idx])
                 n_added += 1
 
-    print(f"angles = {angle_at_position}")
     sec_fibers_list = []
     for node in sec_fiber_positions:
         sec_fiber = temporal.Fiber(random_params={"num_points": sec_len,
@@ -961,3 +961,90 @@ def generate_secondary_fibers(datamap_shape, main_fiber, n_secondary, min_dist=1
         sec_fibers_list.append(sec_fiber)
 
     return sec_fibers_list
+
+
+def generate_synapses_on_fiber(datamap_shape, main_fiber, n_syn, min_dist, synapse_scale=(5, 10)):
+    """
+    Instead of generating a fiber with synapses on it, I will simply generate synapses on a fiber
+    :return:
+    """
+    n_added = 0
+    synapse_positions = numpy.empty((0, 2))
+    n_synapses = int(random.uniform(*n_syn))
+    while n_added != n_synapses:
+        sampled_node = numpy.asarray(random.sample(list(main_fiber.nodes_position), 1)[0].astype(int))
+        if numpy.less_equal(sampled_node, 0).any() or \
+                numpy.greater_equal(sampled_node, datamap_shape - numpy.ones((1, 1))).any():
+            continue
+        if n_added == 0:
+            synapse_positions = numpy.append(synapse_positions, sampled_node)
+            synapse_positions = numpy.expand_dims(synapse_positions, 0).astype(int)
+            n_added += 1
+            continue
+        # comparer la distance du point samplé à tous les points dans la liste
+        # vérifier qu'elle est plus grande que min_distance pour tous les points déjà présents,
+        # si c'est le cas, l'ajouter à la liste, sinon continuer le while :)
+        else:
+            sample_to_verify = numpy.expand_dims(numpy.copy(sampled_node), axis=0).astype(int)
+            synapse_positions = numpy.append(synapse_positions, sample_to_verify, axis=0).astype(int)
+            distances = cdist(synapse_positions, synapse_positions)
+            distances[n_added, n_added] = min_dist + 1
+            if numpy.less_equal(distances[n_added, :], min_dist).any():
+                # at least 1 elt is closer than 10 pixels to an already present elt so remove it :)
+                synapse_positions = numpy.delete(synapse_positions, n_added, axis=0)
+            else:
+                # good to add to the list
+                n_added += 1
+
+    synapse_list = []
+    for node in synapse_positions:
+        polygon = temporal.Polygon(random_params={"pos": [node, node],
+                                                  "scale": synapse_scale})
+        synapse_list.append(polygon)
+
+    return synapse_list
+
+
+def generate_synaptic_fibers(image_shape, main_nodes, n_sec_fibers, n_synapses, min_fiber_dist=3, min_synapse_dist=1,
+                             sec_fiber_len=(10, 20), synapse_scale=(5, 5)):
+    """
+    This function wraps up the generation of fibers with secondary branches and synapses in a neat little package :)
+    - Add variable number of synapses, distances, ?
+    - Add "position identifiers" to the synapses so I can easily make them flash after
+    - ??
+    :return:
+    """
+
+    # generate an empty image
+    image = numpy.zeros(image_shape)
+
+    # generate the main fiber
+    min_nodes, max_nodes = main_nodes[0], main_nodes[1]
+    min_array, max_array = numpy.asarray((min_nodes, min_nodes)), numpy.asarray((max_nodes, max_nodes))
+    fibre_rand = temporal.Fiber(random_params={"num_points": (min_nodes, max_nodes),
+                                               "pos": [numpy.zeros((1, 2)) + min_array,
+                                                       image.shape - max_array],
+                                               "scale": (1, 5)})
+
+    # generate secondary fibers
+    # ces params là devraient être ajustables
+    sec_fibers = generate_secondary_fibers(image_shape, fibre_rand, n_sec_fibers, min_fiber_dist, sec_len=sec_fiber_len)
+
+    # generate synapses attached to the secondary fibers
+    synapses_lists = []
+    for secondary_fiber in sec_fibers:
+        ith_fiber_synapses = generate_synapses_on_fiber(image_shape, secondary_fiber, n_synapses, min_synapse_dist,
+                                                        synapse_scale=synapse_scale)
+        synapses_lists.append(ith_fiber_synapses)
+
+    roi = ((0, 0), image_shape)  # jtrouve que la façon de gérer la shape de l'ensemble est weird
+    ensemble_test = temporal.Ensemble(roi=roi)
+    ensemble_test.append(fibre_rand)
+    for idx, sec_fiber in enumerate(sec_fibers):
+        ensemble_test.append(sec_fiber)
+        for synapse in synapses_lists[idx]:
+            ensemble_test.append(synapse)
+
+    frame = ensemble_test.return_frame()
+
+    return frame
--- synapse_generation.py
+++ synapse_generation.py
@@ -66,31 +66,56 @@ frame = ensemble_test.return_frame()
 plt.imshow(frame)
 plt.show()
 """
-#--------------------------------------- Fiber -------------------------------------------------------------------------
-
-
+#--------------------------------------- Fiber + sec fibers + synapses -------------------------------------------------
+"""
 # ici je veux générer une fibre et trouver comment je peux générer les sous fibres perpendiculaires-ish à la principale
 
 image = np.zeros((256, 256))
 
-min_nodes, max_nodes = 100, 200
+# generate the main fiber
+# si je mets 200, 256 ça fait que des fois il finit jamais, ? pt pcq 256 >= image.shape
+# il faut que je mette max_nodes < image.shape sinon ça fuck
+# aussi je pense que ce que je fais live ne marchera pas sur une image pas carrée?
+# cette partie là faudrait que je repasse dessus c'est weird
+min_nodes, max_nodes = 200, 255   # si je mets 200, 300 ça fait que des fois il finit jamais, ? pt pcq 300 > image.shape
 min_array, max_array = np.asarray((min_nodes, min_nodes)), np.asarray((max_nodes, max_nodes))
 fibre_rand = temporal.Fiber(random_params={"num_points": (min_nodes, max_nodes),
                                                 "pos": [np.zeros((1, 2)) + min_array,
                                                         image.shape - max_array],
                                                 "scale": (1, 5)})
 
-sec_fibers = utils.generate_secondary_fibers(image.shape, fibre_rand, 5, 3, sec_len=(3, 8))
+# generate the secondary fibers branching out from the main fiber
+sec_fibers = utils.generate_secondary_fibers(image.shape, fibre_rand, 5, 3, sec_len=(10, 20))
 
-print(f"min angle = {np.min(fibre_rand.angles)}")
-print(f"max angle = {np.max(fibre_rand.angles)}")
+# generate synapses attached to the secondary fibers
+synapses_lists = []
+for secondary_fiber in sec_fibers:
+    ith_fiber_synapses = utils.generate_synapses_on_fiber(image.shape, secondary_fiber, 2, 1, synapse_scale=(5, 5))
+    synapses_lists.append(ith_fiber_synapses)
+
 n_frames = 1
 roi = ((0, 0), image.shape)   # jtrouve que la façon de gérer la shape de l'ensemble est weird
 ensemble_test = temporal.Ensemble(roi=roi)
 ensemble_test.append(fibre_rand)
-for sec_fiber in sec_fibers:
+for idx, sec_fiber in enumerate(sec_fibers):
     ensemble_test.append(sec_fiber)
+    for synapse in synapses_lists[idx]:
+        ensemble_test.append(synapse)
+
 frame = ensemble_test.return_frame()
 
 plt.imshow(frame)
+plt.show()
+"""
+#--------------------------------------- Testing encompasing function --------------------------------------------------
+
+# testing my generation function
+# des fois il fait juste jamais finir? Je sais pas trop pourquoi, mais mon hypothèse c'est
+# qu'il essait de placer des trucs out of bounds et il reste pris dans une loop infinie
+# je pourrais pt ajouter de quoi que genre s'il fait + que X iter de la loop pour placer une
+# fibre / synapse et qu'il ne parvient pas il fait juste give up ou qqchose
+func_frame = utils.generate_synaptic_fibers((256, 256), (100, 255), (3, 10), (2, 5))
+# func_frame = utils.generate_synaptic_fibers((256, 256), (200, 255), 5, 2)
+
+plt.imshow(func_frame)
 plt.show()
