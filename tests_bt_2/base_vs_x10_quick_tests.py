a18818096943975e4cf63dd314e8942e80221f57
Benoit Turcotte <benoiturcotte@gmail.com>
2022-01-12 11:19:27 -0500

    working on finding the right fluo params, I think I found good params for x10 more molecs and x100 times more molecs

--- tests_bt_2/base_vs_x10_quick_tests.py
+++ tests_bt_2/base_vs_x10_quick_tests.py
@@ -9,6 +9,7 @@ from pysted import exp_data_gen as dg
 from gym_sted.rewards.objectives_timed import find_nanodomains, Signal_Ratio, Resolution
 from gym_sted.utils import get_foreground
 import metrics
+import time
 
 
 default_egfp = {
@@ -33,23 +34,60 @@ default_egfp = {
 }
 
 # fluo params dict to modify and test
-egfp = {
+egfp_x10 = {
+    # wavelength of the fluorophores, doesn't seem to change much if we stay close-ish (very large) to the beam
+    # wavelengths, but going too far (i.e. 100e-9) renders the fluorophores much less "potent"
     "lambda_": 535e-9,
-    "qy": 0.6,
+    "qy": 0.6,   # increasing increases the number of photons, decreasing decreases it
     "sigma_abs": {
-        488: 0.08e-21,
+        # beam - molecule interaction for excitation
+        # modifying 575 (STED) doesn't seem to have much impact?
+        # increasing 488 (EXC) increases number of photns, while decreasing it decreases the number of photons
+        488: 0.03e-22,
         575: 0.02e-21
     },
     "sigma_ste": {
-        575: 3.0e-22,
+        # beam - molecule interaction for STED
+        # decreasing the value decreases the STED effect, making the img more confocal for same STED power
+        # increasing the value increases STED effect without increasing photobleaching cause by STED
+        575: 2.0e-22,
     },
     "sigma_tri": 10.14e-21,
-    "tau": 3e-09,
-    "tau_vib": 1.0e-12,
-    "tau_tri": 1.2e-6,
+    "tau": 3e-09,   # decreasing reduces STED effect while leaving its photobleaching the same, increasing does ?
+    "tau_vib": 1.0e-12,   # decreasing reduces STED effect while leaving its photobleaching the same, increasing does ?
+    "tau_tri": 1.2e-6,   # decreasing decreases photobleaching, increasing increases photobleaching ?
     "phy_react": {
-        488: 0.008e-6,
-        575: 0.008e-9
+        488: 0.008e-6,   # photobleaching caused by exc beam, lower = less photobleaching
+        575: 0.008e-9    # photobleaching cuased by sted beam, lower = less photobleaching
+    },
+    "k_isc": 0.48e+6,
+}
+
+egfp_x100 = {
+    # wavelength of the fluorophores, doesn't seem to change much if we stay close-ish (very large) to the beam
+    # wavelengths, but going too far (i.e. 100e-9) renders the fluorophores much less "potent"
+    "lambda_": 535e-9,
+    "qy": 0.6,   # increasing increases the number of photons, decreasing decreases it
+    "sigma_abs": {
+        # beam - molecule interaction for excitation
+        # modifying 575 (STED) doesn't seem to have much impact?
+        # increasing 488 (EXC) increases number of photns, while decreasing it decreases the number of photons
+        488: 0.0275e-23,
+        575: 0.02e-21
+    },
+    "sigma_ste": {
+        # beam - molecule interaction for STED
+        # decreasing the value decreases the STED effect, making the img more confocal for same STED power
+        # increasing the value increases STED effect without increasing photobleaching cause by STED
+        575: 2.0e-22,
+    },
+    "sigma_tri": 10.14e-21,
+    "tau": 3e-09,   # decreasing reduces STED effect while leaving its photobleaching the same, increasing does ?
+    "tau_vib": 1.0e-12,   # decreasing reduces STED effect while leaving its photobleaching the same, increasing does ?
+    "tau_tri": 1.2e-6,   # decreasing decreases photobleaching, increasing increases photobleaching ?
+    "phy_react": {
+        488: 0.008e-6,   # photobleaching caused by exc beam, lower = less photobleaching
+        575: 0.008e-9    # photobleaching cuased by sted beam, lower = less photobleaching
     },
     "k_isc": 0.48e+6,
 }
@@ -71,8 +109,11 @@ fluo_default = base.Fluorescence(**default_egfp)
 microscope_default = base.Microscope(laser_ex, laser_sted, detector, objective, fluo_default, load_cache=True)
 i_ex, i_sted, _ = microscope_default.cache(pixelsize, save_cache=True)
 
-fluo_test = base.Fluorescence(**egfp)
-microscope_test = base.Microscope(laser_ex, laser_sted, detector, objective, fluo_test, load_cache=True)
+fluo_x10 = base.Fluorescence(**egfp_x10)
+microscope_x10 = base.Microscope(laser_ex, laser_sted, detector, objective, fluo_x10, load_cache=True)
+
+fluo_x100 = base.Fluorescence(**egfp_x100)
+microscope_x100 = base.Microscope(laser_ex, laser_sted, detector, objective, fluo_x100, load_cache=True)
 
 n_molecs_base = 5
 n_molecs_in_domain_base = 135
@@ -91,26 +132,52 @@ shroom_x10.add_nanodomains(10, min_dist_nm=min_dist, n_molecs_in_domain=n_molecs
 dmap_x10 = base.TemporalDatamap(shroom_x10.frame, pixelsize, shroom_x10)
 dmap_x10.set_roi(i_ex, "max")
 
+shroom_x100 = dg.Synapse(n_molecs_base * multiplier ** 2, mode="mushroom", seed=42)
+shroom_x100.add_nanodomains(10, min_dist_nm=min_dist, n_molecs_in_domain=n_molecs_in_domain_base * multiplier ** 2,
+                            seed=42, valid_thickness=7)
+
+dmap_x100 = base.TemporalDatamap(shroom_x100.frame, pixelsize, shroom_x100)
+dmap_x100.set_roi(i_ex, "max")
+
 print(f"default acq 1 ...")
+start_time = time.time()
 acq_default_1, bleached_default_1, _ = microscope_default.get_signal_and_bleach(dmap_base, dmap_base.pixelsize,
                                                                                 **optim_params,
                                                                                 bleach=True, update=True)
+print(f"t = {time.time() - start_time} s")
 print(f"default acq 2 ...")
+start_time = time.time()
 acq_default_2, bleached_default_2, _ = microscope_default.get_signal_and_bleach(dmap_base, dmap_base.pixelsize,
                                                                                 **optim_params,
                                                                                 bleach=True, update=True)
+print(f"t = {time.time() - start_time} s")
 print(f"x10 acq 1 ...")
-acq_x10_1, bleached_x10_1, _ = microscope_test.get_signal_and_bleach(dmap_x10, dmap_x10.pixelsize,
+start_time = time.time()
+acq_x10_1, bleached_x10_1, _ = microscope_x10.get_signal_and_bleach(dmap_x10, dmap_x10.pixelsize,
                                                                      **optim_params,
                                                                      bleach=True, update=True)
+print(f"t = {time.time() - start_time} s")
 print(f"x10 acq 2 ...")
-acq_x10_2, bleached_x10_2, _ = microscope_test.get_signal_and_bleach(dmap_x10, dmap_x10.pixelsize,
+start_time = time.time()
+acq_x10_2, bleached_x10_2, _ = microscope_x10.get_signal_and_bleach(dmap_x10, dmap_x10.pixelsize,
                                                                      **optim_params,
                                                                      bleach=True, update=True)
-
+print(f"t = {time.time() - start_time} s")
+print(f"x100 acq 1 ...")
+start_time = time.time()
+acq_x100_1, bleached_x100_1, _ = microscope_x100.get_signal_and_bleach(dmap_x100, dmap_x100.pixelsize,
+                                                                       **optim_params,
+                                                                       bleach=True, update=True)
+print(f"t = {time.time() - start_time} s")
+print(f"x100 acq 2 ...")
+start_time = time.time()
+acq_x100_2, bleached_x100_2, _ = microscope_x100.get_signal_and_bleach(dmap_x100, dmap_x100.pixelsize,
+                                                                       **optim_params,
+                                                                       bleach=True, update=True)
+print(f"t = {time.time() - start_time} s")
 
 cbar_dict = {"fraction": 0.05, "pad": 0.05}
-fig, axes = plt.subplots(4, 2)
+fig, axes = plt.subplots(4, 3)
 
 def_acq_imshow_1 = axes[0, 0].imshow(acq_default_1)
 axes[0, 0].set_title(f"Default parameters \n Acq 1")
@@ -120,11 +187,18 @@ x10_acq_imshow_1 = axes[0, 1].imshow(acq_x10_1)
 axes[0, 1].set_title(f"x10 parameters \n Acq 1")
 fig.colorbar(x10_acq_imshow_1, ax=axes[0, 1], **cbar_dict)
 
+x100_acq_imshow_1 = axes[0, 2].imshow(acq_x100_1)
+axes[0, 2].set_title(f"x100 parameters \n Acq 1")
+fig.colorbar(x100_acq_imshow_1, ax=axes[0, 2], **cbar_dict)
+
 def_bleached_imshow_1 = axes[1, 0].imshow(bleached_default_1["base"][dmap_base.roi])
 fig.colorbar(def_bleached_imshow_1, ax=axes[1, 0], **cbar_dict)
 
 x10_bleached_imshow_1 = axes[1, 1].imshow(bleached_x10_1["base"][dmap_x10.roi])
 fig.colorbar(x10_bleached_imshow_1, ax=axes[1, 1], **cbar_dict)
+
+x100_bleached_imshow_1 = axes[1, 2].imshow(bleached_x100_1["base"][dmap_x100.roi])
+fig.colorbar(x100_bleached_imshow_1, ax=axes[1, 2], **cbar_dict)
 
 def_acq_imshow_2 = axes[2, 0].imshow(acq_default_2)
 axes[2, 0].set_title(f"Acq 2")
@@ -134,10 +208,17 @@ x10_acq_imshow_2 = axes[2, 1].imshow(acq_x10_2)
 axes[2, 1].set_title(f"Acq 2")
 fig.colorbar(x10_acq_imshow_2, ax=axes[2, 1], **cbar_dict)
 
+x100_acq_imshow_2 = axes[2, 2].imshow(acq_x100_2)
+axes[2, 2].set_title(f"Acq 2")
+fig.colorbar(x100_acq_imshow_2, ax=axes[2, 2], **cbar_dict)
+
 def_bleached_imshow_2 = axes[3, 0].imshow(bleached_default_2["base"][dmap_base.roi])
 fig.colorbar(def_bleached_imshow_2, ax=axes[3, 0], **cbar_dict)
 
 x10_bleached_imshow_2 = axes[3, 1].imshow(bleached_x10_2["base"][dmap_x10.roi])
 fig.colorbar(x10_bleached_imshow_2, ax=axes[3, 1], **cbar_dict)
+
+x100_bleached_imshow_2 = axes[3, 2].imshow(bleached_x100_2["base"][dmap_x100.roi])
+fig.colorbar(x100_bleached_imshow_2, ax=axes[3, 2], **cbar_dict)
 
 plt.show()
--- tests_bt_2/future_bleach_verif.py
+++ tests_bt_2/future_bleach_verif.py
@@ -11,34 +11,6 @@ from gym_sted.utils import get_foreground
 import metrics
 
 
-"""
-faire des acqs back to back avec les params optimaux et voir le f1 score à chaque acq
-"""
-
-
-default_egfp = {
-    "lambda_": 535e-9,
-    "qy": 0.6,
-    "sigma_abs": {
-        488: 0.08e-21,
-        575: 0.02e-21
-    },
-    "sigma_ste": {
-        575: 3.0e-22,
-    },
-    "sigma_tri": 10.14e-21,
-    "tau": 3e-09,
-    "tau_vib": 1.0e-12,
-    "tau_tri": 1.2e-6,
-    "phy_react": {
-        488: 0.008e-5,
-        575: 0.008e-8
-    },
-    "k_isc": 0.48e+6
-}
-
-# CES PARAMS LÀ ONT LAIR GOOD, IL ME RESTE À CHECKER LE BLEACH FUTUR SI IL SE COMPORTE COMME JE VOUDRAIS
-# fluo params dict to modify and test
 egfp_x10 = {
     # wavelength of the fluorophores, doesn't seem to change much if we stay close-ish (very large) to the beam
     # wavelengths, but going too far (i.e. 100e-9) renders the fluorophores much less "potent"
@@ -62,7 +34,7 @@ egfp_x10 = {
     "tau_vib": 1.0e-12,   # decreasing reduces STED effect while leaving its photobleaching the same, increasing does ?
     "tau_tri": 1.2e-6,   # decreasing decreases photobleaching, increasing increases photobleaching ?
     "phy_react": {
-        488: 0.008e-6,   # photobleaching caused by exc beam, lower = less photobleaching
+        488: 0.0008e-6,   # photobleaching caused by exc beam, lower = less photobleaching
         575: 0.008e-9    # photobleaching cuased by sted beam, lower = less photobleaching
     },
     "k_isc": 0.48e+6,
@@ -91,7 +63,7 @@ egfp_x100 = {
     "tau_vib": 1.0e-12,   # decreasing reduces STED effect while leaving its photobleaching the same, increasing does ?
     "tau_tri": 1.2e-6,   # decreasing decreases photobleaching, increasing increases photobleaching ?
     "phy_react": {
-        488: 0.008e-6,   # photobleaching caused by exc beam, lower = less photobleaching
+        488: 0.0008e-6,   # photobleaching caused by exc beam, lower = less photobleaching
         575: 0.008e-9    # photobleaching cuased by sted beam, lower = less photobleaching
     },
     "k_isc": 0.48e+6,
@@ -101,6 +73,11 @@ optim_params = {
     "pdt": 10e-6,
     "p_ex": 0.25e-3,
     "p_sted": 87.5e-3
+}
+confocal_params = {
+    "pdt": 10e-6,
+    "p_ex": 0.05e-3,
+    "p_sted": 0.0
 }
 
 pixelsize = 20e-9
@@ -119,57 +96,62 @@ microscope_x100 = base.Microscope(laser_ex, laser_sted, detector, objective, flu
 i_ex, i_sted, _ = microscope_x100.cache(pixelsize, save_cache=True)
 
 n_molecs_base = 5
-n_molecs_in_domain_base = 135
+n_molecs_in_domain_base = 0   # 135
 min_dist = 50
 multiplier = 10
-
-shroom_base = dg.Synapse(n_molecs_base, mode="mushroom", seed=42)
-shroom_base.add_nanodomains(10, min_dist_nm=min_dist, n_molecs_in_domain=n_molecs_in_domain_base, seed=42,
-                            valid_thickness=7)
-dmap_base = base.TemporalDatamap(shroom_base.frame, pixelsize, shroom_base)
-dmap_base.set_roi(i_ex, "max")
 
 shroom_x10 = dg.Synapse(n_molecs_base * multiplier, mode="mushroom", seed=42)
 shroom_x10.add_nanodomains(10, min_dist_nm=min_dist, n_molecs_in_domain=n_molecs_in_domain_base * multiplier, seed=42,
                            valid_thickness=7)
-dmap_x10 = base.TemporalDatamap(shroom_x10.frame, pixelsize, shroom_x10)
+dmap_x10 = base.TemporalSynapseDmap(shroom_x10.frame, pixelsize, shroom_x10)
 dmap_x10.set_roi(i_ex, "max")
+dmap_x10.create_t_stack_dmap_smooth(2000000, delay=2)
 
 shroom_x100 = dg.Synapse(n_molecs_base * multiplier ** 2, mode="mushroom", seed=42)
 shroom_x100.add_nanodomains(10, min_dist_nm=min_dist, n_molecs_in_domain=n_molecs_in_domain_base * multiplier ** 2,
                             seed=42, valid_thickness=7)
-dmap_x100 = base.TemporalDatamap(shroom_x100.frame, pixelsize, shroom_x100)
+dmap_x100 = base.TemporalSynapseDmap(shroom_x100.frame, pixelsize, shroom_x100)
 dmap_x100.set_roi(i_ex, "max")
+dmap_x100.create_t_stack_dmap_smooth(2000000, delay=2)
 
-nd_gt_positions = np.copy(np.array(dmap_base.synapses.nanodomains_coords))
+mode = "x10"
+if mode == "x10":
+    chosen_dmap = dmap_x10
+    chosen_microscope = microscope_x10
+elif mode == "x100":
+    chosen_dmap = dmap_x100
+    chosen_microscope = microscope_x100
+else:
+    chosen_dmap = None
+    chosen_microscope = None
 
-n_acqs = 3
 acqs = []
-bleached = []
-fig, axes = plt.subplots(2, n_acqs)
-dmap_copy = np.copy(dmap_x100.whole_datamap[dmap_x100.roi])
-for i in range(n_acqs):
-    print(f"acq {i+1} of {n_acqs}")
-    acq, bleached_dmap, _ = microscope_x100.get_signal_and_bleach(dmap_x100, dmap_x100.pixelsize,
-                                                             **optim_params,
-                                                             bleach=True, update=True, bleach_mode="proportional")
+for t in range(chosen_dmap.flash_tstack.shape[0]):
+    for i in range(7):
+        chosen_dmap.update_whole_datamap(i)
+        chosen_dmap.update_dicts({"flashes": i})
+        plt.imshow(chosen_dmap.whole_datamap[chosen_dmap.roi])
+        plt.title(f"t = {i},"
+                  f"\n max = {np.max(chosen_dmap.whole_datamap[chosen_dmap.roi])}")
+        plt.show()
 
-    acqs.append(np.copy(acq))
-    bleached.append(np.copy(bleached_dmap["base"][dmap_base.roi]))
+    chosen_dmap.update_whole_datamap(t)
+    chosen_dmap.update_dicts({"flashes": t})
 
-    # COMPUTE LE F1_SCORE ICI :)
-    nd_guess_positions = find_nanodomains(acq, dmap_base.pixelsize)
-    detector = metrics.CentroidDetectionError(nd_gt_positions, nd_guess_positions, 2,
-                                              algorithm="hungarian")
-    f1_score = detector.f1_score
-    photobleaching = (np.sum(dmap_copy) - np.sum(bleached[i])) / np.sum(dmap_copy)
+    acq, bleached, _ = chosen_microscope.get_signal_and_bleach(chosen_dmap, chosen_dmap.pixelsize, **confocal_params,
+                                                               bleach=True, update=True, bleach_mode="proportional")
 
-    imshow_acq = axes[0, i].imshow(acqs[i])
-    axes[0, i].set_title(f"acq {i}, \n f1_score = {f1_score}")
-    fig.colorbar(imshow_acq, ax=axes[0, i], fraction=0.05, pad=0.05)
+    acqs.append(acq)
 
-    imshow_bleached = axes[1, i].imshow(bleached[i])
-    axes[1, i].set_title(f"bleached {i}, \n photobleaching = {round(photobleaching, 4)}")
-    fig.colorbar(imshow_bleached, ax=axes[1, i], fraction=0.05, pad=0.05)
+    fig, axes = plt.subplots(1, len(acqs))
+    for j, acq_to_show in enumerate(acqs):
+        if len(acqs) > 1:
+            axes[j].imshow(acq_to_show, vmin=0, vmax=np.max(np.asarray(acqs)))
+            axes[j].set_title(f"t = {j}")
+            fig.suptitle(f"ratio between 2 latest acqs = {np.sum(acqs[-1]) / np.sum(acqs[-2])}")
+        else:
+            axes.imshow(acq_to_show, vmin=0, vmax=np.max(np.asarray(acqs)))
+            axes.set_title(f"t = {j}")
+    plt.show()
+    plt.close(fig)
 
-plt.show()
